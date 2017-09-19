aa3086595f23e2d3b4ddc686b3b6c13ec3650178
skzheng <simonkzheng@gmail.com>
2017-09-19 10:57:15 -0400

    Revert "Dev"

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
- 
-document.addEventListener('DOMContentLoaded', function() {
-  // ReactDOM.render(
-  //   React.createElement(Counter),
-  //   document.getElementById('mount')
-  // );
-});
