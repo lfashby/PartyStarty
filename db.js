01dcd159885bd1ebda729d575cf97f33354278a7
Lucas Ashby <lfeashby@gmail.com>
2017-10-03 16:04:09 -0400

    Added nodemon and our own db

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoURI = 'mongodb://bryan:bryan@ds141524.mlab.com:41524/partydb'; 
+// mongoURI = 'mongodb://bryan:bryan@ds141524.mlab.com:41524/partydb'; 
+mongoURI = 'mongodb://lfa:lfa@ds161164.mlab.com:61164/party-starty';
 mongoose.connect(mongoURI, {useMongoClient: true});
 
 var db = mongoose.connection;
