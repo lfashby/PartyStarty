5cf31b37f208e6ddd6412dfcaaaf65bf8f61a798
Dustin <dustykim@gmail.com>
2017-10-03 17:17:23 -0400

    create subfolder and basic view for event page

---
+++ src/event/eventVoter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class EventVoter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  
+
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    )
+  }
+}
+
+export default EventVoter;
