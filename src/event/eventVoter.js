7e1836945172bec1f3fce4474d808366c342e1f2
Dustin <dustykim@gmail.com>
2017-10-04 13:39:39 -0400

    dropdown added

--- src/event/eventVoter.js
+++ src/event/eventVoter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 class EventVoter extends React.Component {
   constructor(props) {
     super(props);
@@ -23,16 +24,38 @@ class EventVoter extends React.Component {
   render() {
     return (
       <div id='voterbox'>
-        
+      
+      <form>
         <div id='movieoption'>
           {this.state.movies[0].title}
+         
+            <select value={this.state.value} onChange={this.handleChange}>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
         </div>
         <div id='movieoption'>
           {this.state.movies[1].title}
+          <select value={this.state.value} onChange={this.handleChange}>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
         </div>
         <div id='movieoption'>
           {this.state.movies[2].title}
+          <select value={this.state.value} onChange={this.handleChange}>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
         </div>
+        <input type="submit" value="Submit" />
+        </form>
       </div>
     )
   }
