e75c5ed77b454ed3293cd951c667a6d5507b640e
Lucas Ashby <lfeashby@gmail.com>
2017-10-07 10:45:01 -0400

    Almost final styling for home

--- src/event/eventListEntry.js
+++ src/event/eventListEntry.js
@@ -31,19 +31,17 @@ grabFilmPoster() {
     return (
       <div className="video-list-entry">
         <li onClick={() => this.grabFilmPoster()} className="list-group-item">
-        {this.props.event.eventTitle}
-        <br></br>
-        Location: {this.props.event.eventLocation}
-        <br></br>
-          Date: {this.props.event.eventDate.slice(0, 15)} - {this.props.event.eventTime}
+        <h4 className="homeText">{this.props.event.eventTitle}</h4>
+        <p className="homeText">Location: {this.props.event.eventLocation}</p>
+        <p className="homeText">Date: {this.props.event.eventDate.slice(0, 15)} - {this.props.event.eventTime}</p>
         { this.state.posters.length ? (
           <div className="postersContainer">
-            <br></br>
-            <p>Hosted by: {this.props.event.eventHostName}</p>
-            <p>Description: {this.props.event.eventDesc}</p>
+            <p className="homeText">Hosted by: {this.props.event.eventHostName}</p>
+            <p className="homeText">Description: {this.props.event.eventDesc}</p>
             <img className="posters" src={`https://image.tmdb.org/t/p/w500${this.state.posters[0].poster}`} />
             <img className="posters" src={`https://image.tmdb.org/t/p/w500${this.state.posters[1].poster}`} />
             <img className="posters" src={`https://image.tmdb.org/t/p/w500${this.state.posters[2].poster}`} />
+            <br></br>
             <Link to='/eventpage'>
               <button onClick={this.props.setLookingAtEvent} value={this.props.event._id} type="button" className="btn">Checkout the Event</button>
             </Link>
