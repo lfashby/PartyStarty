e610fab5c20eff2ce8095fed48f2153ce91911c4
Lucas Ashby <lfeashby@gmail.com>
2017-10-07 13:33:09 -0400

    Added styling to invite page

--- src/eventCreator/Invite.js
+++ src/eventCreator/Invite.js
@@ -6,16 +6,18 @@ import {Link} from 'react-router-dom';
 const Invite = (props) => (
   <div>
     <div className="createpage">
-        <h2 className="subH">Invite your friends, if you'd like</h2>
+      <div className="invitesHeader">
+        <h2 className="subH">Invite your friends, if you'd like:</h2>
         <form>
         <Link to='/eventpage'>
-          <button onClick={props.setLookingAtEvent} value={props.eventId} type="button" className="btn">Finish Creating Event</button>
+          <button onClick={props.setLookingAtEvent} value={props.eventId} type="button" className="btn finishCreatingButton">Finish Creating Event</button>
         </Link>
         </form>
-        <form onSubmit={props.handleFriends}>
-          <input value={props.friendValue} onChange={props.handleFriendChange} className="form-control" type="text" placeholder="Enter username" />
-          <input className="btn btn-secondary btn-lg textarea" type="submit" value="Add user" />
-        </form>
+      </div>
+      <form onSubmit={props.handleFriends}>
+        <input value={props.friendValue} onChange={props.handleFriendChange} className="form-control" type="text" placeholder="Enter username" />
+        <input className="btn btn-secondary btn-lg textarea" type="submit" value="Add user" />
+      </form>
       <ul>
         {props.friends.map((friend, i) => {
           return <InviteEntry friend={friend} 
