6e620d01d79b82969c650a6a250518b8b00dd983
Dustin <dustykim@gmail.com>
2017-10-07 15:46:57 -0400

    fixing vote button

--- src/event/eventVoter.js
+++ src/event/eventVoter.js
@@ -12,83 +12,88 @@ const EventVoter = (props) => (
       at {props.event.eventLocation}
     </div>
 
-    <form className='voterBox'>
-      <div className='movieOption'>
-        {props.movies.length > 0 ? 
-        <div className='boxItem'>
-          <img 
-            className="img-thumbnail" 
-            src={`https://image.tmdb.org/t/p/w500${props.movies[0].poster}`} 
-          />
-        </div> : ''}
-        <select value={props.firstRating} onChange={props.handleFirstRating}>
-          <option value="0">0</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
-      </div>
-      <div className='movieOption'>
-        {props.movies.length > 0 ? 
-        <div className='boxItem'>
-          <img 
-            className="img-thumbnail" 
-            src={`https://image.tmdb.org/t/p/w500${props.movies[1].poster}`} 
-          />
-        </div> : ''}
-        <select value={props.secondRating} onChange={props.handleSecondRating}>
-          <option value="0">0</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
-      </div>
-      <div className='movieOption'>
-        {props.movies.length > 0 ? 
-        <div className='boxItem'>
-          <img 
-            className="img-thumbnail" 
-            src={`https://image.tmdb.org/t/p/w500${props.movies[2].poster}`} 
-          />
-        </div> : ''}
-        <select value={props.thirdRating} onChange={props.handleThirdRating}>
-          <option value="0">0</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
-      </div>
-    </form>
-    
-    <button
+    <div >
+      <form className="voterBox">
+        <div className='movieOption'>
+          {props.movies.length > 0 ? 
+          <div className='boxItem'>
+            <img 
+              className="img-thumbnail" 
+              src={`https://image.tmdb.org/t/p/w500${props.movies[0].poster}`} 
+            />
+          </div> : ''}
+          <select value={props.firstRating} onChange={props.handleFirstRating}>
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+        <div className='movieOption'>
+          {props.movies.length > 0 ? 
+          <div className='boxItem'>
+            <img 
+              className="img-thumbnail" 
+              src={`https://image.tmdb.org/t/p/w500${props.movies[1].poster}`} 
+            />
+          </div> : ''}
+          <select value={props.secondRating} onChange={props.handleSecondRating}>
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+        <div className='movieOption'>
+          {props.movies.length > 0 ? 
+          <div className='boxItem'>
+            <img 
+              className="img-thumbnail" 
+              src={`https://image.tmdb.org/t/p/w500${props.movies[2].poster}`} 
+            />
+          </div> : ''}
+          <select value={props.thirdRating} onChange={props.handleThirdRating}>
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+      </form>
+
+      <button
         type="button" 
         className="btn btn-primary" 
         id="voteButton"
         onClick={(e) => {props.submitRatings(e)}} 
       >
-      Vote
-    </button>
+        Vote
+      </button>
+
+    </div>
+
+   
 
     {props.justVoted ? 
     <div className="alert alert-success" role="alert">
